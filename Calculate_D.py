e91244cf54d2e96cb5883178f71ed06dea3818a0
JMBarberio-nate <joseph@nate.tech>
2021-12-21 13:04:01 -0500

    file name consistency

--- Calculate_D.py
+++ Calculate_D.py
@@ -1,18 +1,19 @@
 import numpy as np
 
+
 def calculate_D(W):
     shape = W.shape
-    if (shape[0] != shape[1]):
+    if shape[0] != shape[1]:
         print("W must be a square matrix")
-        return null
+        return -1
     D = np.zeros(shape)
-    sum = np.sum(W, axis = 0)
+    sum = np.sum(W, axis=0)
     for i in range(shape[0]):
         D[i][i] = sum[i]
     return D
 
-#input = np.array([[3,4,1],[5,6,10],[8,3,9]])
-#output = calculate_D(input)
-#print(input)
-#print(output)
 
+# input = np.array([[3,4,1],[5,6,10],[8,3,9]])
+# output = calculate_D(input)
+# print(input)
+# print(output)

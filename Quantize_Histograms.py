529ae199927561459e33672a845c6237b13855c5
kathfree3 <kfree3@seas.upenn.edu>
2021-12-21 15:12:20 -0500

    quant hists

--- Quantize_Histograms.py
+++ Quantize_Histograms.py
@@ -3,16 +3,10 @@ Step 2 of Algorithm:
 quantize every histogram Ht into K prototypes: 
 P = {p1, . . . , pK}
 
-modules Required:
-
 
 Steps
 - apply vector quantization to the histogram feture vectors classifying them into a 
  dictionary of K prototype features, P = {p1,,,} USING K-means
-
- Inputs
- - Histogram H
- - K 
 
 output: 
 1) the K protoypes aka centroids of the result of the k emeans algo
@@ -24,9 +18,8 @@ import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
 from sklearn import cluster
-from scipy.misc import face
 
-from histogram_gen import frameDeltaGivenPureBackground
+from histogram_gen import *
 
 
 """
@@ -49,23 +42,34 @@ def quantiize(H_arr, K):
   n_samples = len(H_arr)
   X = []
   for Ht in H_arr:
-    n = Ht.reshape((-1, 1))
+    n = np.reshape(Ht, (-1, 1))
     X.append(n)
 
-  print(X)
-  print(X.shape)
-  
-  X = X.reshape((-1, 1))  # We need an (n_sample, n_feature) array
+
+  temp_arr = np.asarray(X)
+  print(temp_arr.shape)
+  temp_arr = np.reshape(temp_arr, (n_samples, -1))
+
   k_means = cluster.KMeans(n_clusters=K)
-  labels = k_means.fit_predict(H_arr)
+  labels = k_means.fit_predict(temp_arr)
   centers = k_means.cluster_centers_
   return labels, centers
 
 
+
 if __name__ == "__main__":
-  img_arr = frameDeltaGivenPureBackground("01_001.avi")
-  print(img_arr)
+  np.random.seed(0)
+  img_array, frame_delta_array, thresh_array = frameDeltaGivenPureBackground("01_001.avi", False)
+  print(thresh_array.shape)
+
+  labels, centers = quantiize(thresh_array, 10)
+  print(labels)
+  print(centers)
+
+
   
+
+
 
 
 
